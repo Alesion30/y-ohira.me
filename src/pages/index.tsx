51d5c73bb9f34f6671a8c14c2fbd8fac24a6ea3e
yuta ohira <alesion90@gmail.com>
2020-12-31 14:45:51 +0900

    lint-stagedのテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,9 @@ const Home: FC = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to<a href="https://nextjs.org">Next.js!</a>
+
+
         </h1>
 
         <p className={styles.description}>
