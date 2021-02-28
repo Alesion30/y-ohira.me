ed8daec55a92369a99f7430f92cd9ccb009fc546
yuta ohira <alesion90@gmail.com>
2021-02-28 20:01:20 +0900

    update: エラーページに画面遷移アニメーションを追加

--- src/layouts/error.tsx
+++ src/layouts/error.tsx
@@ -5,6 +5,7 @@ import LinkCard from '~/components/LinkCard';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
+import { motion } from 'framer-motion';
 
 interface Props {
   statusCode: number;
@@ -21,21 +22,28 @@ const ErrorPageLayout: NextPage<Props> = ({ statusCode, errorMessage }) => {
         <title>{title}</title>
         <meta property="og:title" content={title} />
       </Head>
-      <div style={{ textAlign: 'center', marginTop: 100, marginBottom: 100 }}>
-        <h1>{status}</h1>
-        <p>{error}</p>
-        <Grid container direction="row" justify="center" alignItems="center">
-          <Avatar alt="una" variant="square" src="/images/una.png" style={{ width: 200, height: 300 }} />
-        </Grid>
-        <CustomSpacer height={40} />
-        <Grid container direction="row" justify="space-around" alignItems="center">
-          <LinkCard href="/" title="Home" description="トップページ" />
-          <LinkCard href="/about" title="About" description="経歴・スキルなど" />
-          <LinkCard href="/product" title="Product" description="今まで作ってきたアプリ" />
-          <LinkCard href="/hobby" title="Hobby" description="個人的な趣味" />
-        </Grid>
-        <CustomSpacer height={20} />
-      </div>
+      <motion.div
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.4 }}
+      >
+        <div style={{ textAlign: 'center', marginTop: 100, marginBottom: 100 }}>
+          <h1>{status}</h1>
+          <p>{error}</p>
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Avatar alt="una" variant="square" src="/images/una.png" style={{ width: 200, height: 300 }} />
+          </Grid>
+          <CustomSpacer height={40} />
+          <Grid container direction="row" justify="space-around" alignItems="center">
+            <LinkCard href="/" title="Home" description="トップページ" />
+            <LinkCard href="/about" title="About" description="経歴・スキルなど" />
+            <LinkCard href="/product" title="Product" description="今まで作ってきたアプリ" />
+            <LinkCard href="/hobby" title="Hobby" description="個人的な趣味" />
+          </Grid>
+          <CustomSpacer height={20} />
+        </div>
+      </motion.div>
     </Container>
   );
 };
