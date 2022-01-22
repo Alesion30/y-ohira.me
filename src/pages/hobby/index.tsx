c8d5aa7ecfeb97b0237ff3a1758c145f1f992c2e
Yuta Ohira <alesion90@gmail.com>
2022-01-22 12:59:36 +0900

    :fire: ページを差し替え

--- src/pages/hobby/index.tsx
+++ src/pages/hobby/index.tsx
@@ -1,77 +1,11 @@
-import { FC } from 'react';
-import { NextPage, GetStaticProps } from 'next';
-import Router from 'next/router';
-import Layout from '~/layouts/index';
-import TopSectionStyle from '~/styles/components/TopSection.module.scss';
-import Grid from '@material-ui/core/Grid';
-import { PostList } from '~/models';
-import breakpoint from '~/utils/breakpoint';
-import { Spacer } from '~/components/uiParts/Spacer';
-import { HoverImage } from '~/components/uiParts/HoverImage';
-import { InViewAnimate } from '~/components/uiParts/InViewAnimate';
+import { NextSeo } from 'next-seo';
+import { HobbyPage } from '~/components/pages/hobby';
 
-interface Props {
-  allPostsData: PostList[];
-}
-
-const Hobby: NextPage<Props> = ({ allPostsData }) => {
+export default () => {
   return (
-    <Layout title="HOBBY">
-      <h1 className={TopSectionStyle.title}>
-        <span className={TopSectionStyle.highlight}>HOBBY</span>
-      </h1>
-      <p className={TopSectionStyle.description}>趣味</p>
-      <Spacer height={30} />
-      <Grid container direction="row" justify="center" alignItems="center">
-        {allPostsData.map((post) => (
-          <InViewAnimate key={post.id} delay={300} open={{ opacity: 1, y: 0 }} closed={{ opacity: 0, y: '10%' }}>
-            <ProductImage
-              href={`/hobby/${post.id}`}
-              title={post.title ?? ''}
-              description={post.description ?? ''}
-              src={post.image ?? ''}
-            />
-          </InViewAnimate>
-        ))}
-      </Grid>
-      <Spacer height={80} />
-    </Layout>
+    <>
+      <NextSeo title="HOBBY" />
+      <HobbyPage />
+    </>
   );
 };
-
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData: PostList[] = [];
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-};
-
-interface ProductImageProps {
-  src: string;
-  title: string;
-  description: string;
-  href: string;
-}
-const ProductImage: FC<ProductImageProps> = ({ src, title, description, href }) => {
-  const isSm = breakpoint('sm');
-  const width = isSm ? 400 : 300;
-  return (
-    <HoverImage
-      onClick={() => Router.push(href)}
-      src={src}
-      alt={title}
-      width={width}
-      height={width * 0.75}
-      margin={10}
-      bottom={10}
-      left={20}
-    >
-      <h1 style={{ fontSize: 20 }}>{title}</h1>
-      <p>{description}</p>
-    </HoverImage>
-  );
-};
-
-export default Hobby;
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,84 +1,11 @@
-import { FC } from 'react';
-import { NextPage, GetStaticProps } from 'next';
-import Router from 'next/router';
-import Layout from '~/layouts/index';
-import TopSectionStyle from '~/styles/components/TopSection.module.scss';
-import Grid from '@material-ui/core/Grid';
-import { PostList } from '~/models';
-import breakpoint from '~/utils/breakpoint';
-import { Spacer } from '~/components/uiParts/Spacer';
-import { HoverImage } from '~/components/uiParts/HoverImage';
-import { InViewAnimate } from '~/components/uiParts/InViewAnimate';
+import { NextSeo } from 'next-seo';
+import { ProductPage } from '~/components/pages/product';
 
-interface Props {
-  allPostsData: PostList[];
-}
-
-const Product: NextPage<Props> = ({ allPostsData }) => {
+export default () => {
   return (
-    <Layout title="PRODUCT">
-      <h1 className={TopSectionStyle.title}>
-        <span className={TopSectionStyle.highlight}>PRODUCT</span>
-      </h1>
-      <p className={TopSectionStyle.description}>今まで作ってきたアプリ</p>
-      <Spacer height={30} />
-      <InViewAnimate open={{ opacity: 1, y: 0 }} closed={{ opacity: 0, y: '10%' }}>
-        <p className={TopSectionStyle.description}>個人開発</p>
-      </InViewAnimate>
-      <Grid container direction="row" justify="center" alignItems="center">
-        {allPostsData.map(
-          (post) =>
-            post.type == 2 && (
-              <InViewAnimate key={post.id} delay={300} open={{ opacity: 1, y: 0 }} closed={{ opacity: 0, y: '10%' }}>
-                <ProductImage
-                  href={`/product/${post.id}`}
-                  title={post.title ?? ''}
-                  description={post.description ?? ''}
-                  src={post.image ?? ''}
-                />
-              </InViewAnimate>
-            )
-        )}
-      </Grid>
-      <Spacer height={80} />
-    </Layout>
+    <>
+      <NextSeo title="PRODUCT" />
+      <ProductPage />
+    </>
   );
 };
-
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData: PostList[] = [];
-
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-};
-
-interface ProductImageProps {
-  src: string;
-  title: string;
-  description: string;
-  href: string;
-}
-const ProductImage: FC<ProductImageProps> = ({ src, title, description, href }) => {
-  const isSm = breakpoint('sm');
-  const width = isSm ? 400 : 300;
-  return (
-    <HoverImage
-      onClick={() => Router.push(href)}
-      src={src}
-      alt={title}
-      width={width}
-      height={width * 0.75}
-      margin={10}
-      bottom={10}
-      left={20}
-    >
-      <h1 style={{ fontSize: 20 }}>{title}</h1>
-      <p>{description}</p>
-    </HoverImage>
-  );
-};
-
-export default Product;
