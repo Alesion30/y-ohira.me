31679b82091c261460edd0a1ebd96176aa028ad8
Yuta Ohira <alesion90@gmail.com>
2022-01-26 20:37:08 +0900

    :sparkles: ブログカードを実装

---
+++ src/components/pages/blog/BlogCard/presenter.tsx
@@ -0,0 +1,23 @@
+import { MyCard } from '~/components/uiParts/MyCard';
+import Image from 'next/image';
+import style from './style.module.scss';
+import dayjs from 'dayjs';
+
+export type BlogCardType = {
+  title: string;
+  date: Date;
+  src: string;
+  href: string;
+};
+
+export const BlogCardPresenter: React.VFC<BlogCardType> = ({ title, date, src, href }) => {
+  return (
+    <MyCard href={href}>
+      <Image src={src} width={400} height={400 * 0.75} />
+      <div className={style.wrapper}>
+        <p className={style.date}>{dayjs(date).format('YYYY/MM/DD')}</p>
+        <h3 className={style.title}>{title}</h3>
+      </div>
+    </MyCard>
+  );
+};
