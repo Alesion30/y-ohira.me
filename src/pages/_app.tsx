c79d15b1b488189aacdcfac8407e0e48fe4d271b
Yuta Ohira <alesion90@gmail.com>
2022-01-26 21:30:40 +0900

    :wrench: apolloの設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,38 +6,60 @@ import { AnimatePresence } from 'framer-motion';
 import Head from 'next/head';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../../next-seo.config';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT ?? '',
+});
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+});
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <AnimatePresence exitBeforeEnter initial={true}>
-      <DefaultSeo {...SEO} />
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#008CD6" />
-        <link rel="manifest" href="/manifest.json" />
+      <ApolloProvider client={client}>
+        <DefaultSeo {...SEO} />
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="theme-color" content="#008CD6" />
+          <link rel="manifest" href="/manifest.json" />
 
-        {/* apple-touch-icon */}
-        <link rel="apple-touch-icon" href="/images/icons/icon-72x72.png" sizes="72x72" />
-        <link rel="apple-touch-icon" href="/images/icons/icon-96x96.png" sizes="96x96" />
-        <link rel="apple-touch-icon" href="/images/icons/icon-128x128.png" sizes="128x128" />
-        <link rel="apple-touch-icon" href="/images/icons/icon-144x144.png" sizes="144x144" />
+          {/* apple-touch-icon */}
+          <link rel="apple-touch-icon" href="/images/icons/icon-72x72.png" sizes="72x72" />
+          <link rel="apple-touch-icon" href="/images/icons/icon-96x96.png" sizes="96x96" />
+          <link rel="apple-touch-icon" href="/images/icons/icon-128x128.png" sizes="128x128" />
+          <link rel="apple-touch-icon" href="/images/icons/icon-144x144.png" sizes="144x144" />
 
-        {/* Google Fonts */}
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300&family=Noto+Sans+JP:wght@100&display=swap"
-          rel="stylesheet"
-        />
+          {/* Google Fonts */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300&family=Noto+Sans+JP:wght@100&display=swap"
+            rel="stylesheet"
+          />
 
-        {/* Font Awesome */}
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"
-          integrity="sha256-UzFD2WYH2U1dQpKDjjZK72VtPeWP50NoJjd26rnAdUI="
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <Component {...pageProps} />
+          {/* Font Awesome */}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"
+            integrity="sha256-UzFD2WYH2U1dQpKDjjZK72VtPeWP50NoJjd26rnAdUI="
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </AnimatePresence>
   );
 };
