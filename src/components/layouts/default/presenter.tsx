a99f7bf8ba3278ee3a4999f0711408d3ad0ab25c
Yuta Ohira <alesion90@gmail.com>
2022-01-26 16:02:35 +0900

    :lipstick: レイアウトを修正

--- src/components/layouts/default/presenter.tsx
+++ src/components/layouts/default/presenter.tsx
@@ -1,14 +1,19 @@
 import { Container } from '@material-ui/core';
 import { motion } from 'framer-motion';
+import { Spacer } from '~/components/uiParts/Spacer';
 
 export type DefaultLayoutProps = {
-  children: React.ReactChild;
+  children: React.ReactNode;
 };
 
 export const DefaultLayoutPresenter: React.VFC<DefaultLayoutProps> = ({ children }) => {
   return (
     <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} transition={{ duration: 0.4 }}>
-      <Container maxWidth="md">{children}</Container>
+      <Container maxWidth="md">
+        <Spacer height={100} />
+        {children}
+        <Spacer height={100} />
+      </Container>
     </motion.div>
   );
 };
