fd7f419ac8428fc2409f5bbe5f9e0e4ed25d1c96
Yuta Ohira <alesion90@gmail.com>
2022-01-30 00:11:29 +0900

    :zap: TOPのカードはスケールしないようにした

--- src/components/uiParts/MyCard/presenter.tsx
+++ src/components/uiParts/MyCard/presenter.tsx
@@ -8,9 +8,25 @@ export type MyCardProps = {
   href?: string;
   blank?: boolean;
   className?: string;
+  disableScale?: boolean;
 };
 
-export const MyCardPresenter: React.VFC<MyCardProps> = ({ children, href, blank, className }) => {
+export const MyCardPresenter: React.VFC<MyCardProps> = ({ children, href, blank, className, disableScale }) => {
+  if (disableScale === true) {
+    if (href) {
+      return (
+        <div className={clsx(style.wrapper, className)}>
+          <Link href={href}>
+            <a target={blank ? '_blank' : undefined} rel={blank ? 'noopener noreferrer' : undefined}>
+              {children}
+            </a>
+          </Link>
+        </div>
+      );
+    }
+    return <div className={clsx(style.wrapper, className)}>{!href && children}</div>;
+  }
+
   if (href) {
     return (
       <motion.div whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.0 }} className={clsx(style.wrapper, className)}>
