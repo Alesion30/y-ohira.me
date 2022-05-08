209081a3e80dd16c584fd56af7bfe7765dfb2c9b
Yuta Ohira <alesion90@gmail.com>
2022-05-08 22:32:05 +0900

    💄 ハンバーガメニュー

--- src/components/layouts/default/header/presenter.tsx
+++ src/components/layouts/default/header/presenter.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 import style from './style.module.scss';
 
@@ -10,18 +11,55 @@ export type HeaderProps = {
 
 export const HeaderPresenter: React.FC<HeaderProps> = () => {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  const links = [
+    { href: '/about', title: 'ABOUT' },
+    { href: '/product', title: 'PRODUCT' },
+    { href: '/blog', title: 'BLOG' },
+  ];
+
+  const onToggle = () => setOpen(!open);
+  const onClose = () => setOpen(false);
+
+  // 背景スクロール禁止制御
+  useEffect(() => {
+    document.body.style.overflowY = open ? 'hidden' : '';
+  }, [open]);
+
   return (
     <div>
-      <div className={style.topLink}>
+      <div className={style.topLinkWrapper}>
         <Link href="/">
-          <a>YUTA OHIRA</a>
+          <a className={style.topLink}>YUTA OHIRA</a>
         </Link>
       </div>
-      <div className={style.menuBtnWrapper} onClick={() => setOpen(!open)}>
+
+      {/* ハンバーガーメニューボタン */}
+      <div className={style.menuBtnWrapper} onClick={onToggle}>
         <div className={clsx(style.menuBtn, open && style.menuBtnActive)}>
           <span></span>
           <span></span>
           <span></span>
+        </div>
+      </div>
+
+      {/* ハンバーガーメニュー */}
+      <div className={clsx(style.menuOverlay, open && style.menuOverlayActive)}>
+        <div className={style.menuLinks}>
+          {links.map((link) => {
+            const isActive = router.asPath === link.href;
+            return (
+              <Link key={link.title} href={link.href}>
+                <a
+                  className={clsx(style.menuLink, isActive && style.menuLinkActive)}
+                  onClick={isActive ? onClose : undefined}
+                >
+                  {link.title}
+                </a>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
