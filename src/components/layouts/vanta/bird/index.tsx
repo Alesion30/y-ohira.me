44f9f1c7fefd198a4aff4e3829b05bfcf97366c9
Yuta Ohira <alesion90@gmail.com>
2022-05-08 17:12:22 +0900

    ✨ VantaBirdLayoutを実装

---
+++ src/components/layouts/vanta/bird/index.tsx
@@ -0,0 +1,5 @@
+import { VantaBirdLayoutPresenter, VantaBirdLayoutProps } from './presenter';
+
+export const VantaBirdLayout: React.FC<VantaBirdLayoutProps> = (props) => {
+  return <VantaBirdLayoutPresenter {...props} />;
+};
--- src/components/layouts/vanta/index.ts
+++ src/components/layouts/vanta/index.ts
@@ -1,3 +1,4 @@
+export * from './bird';
 export * from './cloud';
 export * from './globe';
 export * from './waves';
