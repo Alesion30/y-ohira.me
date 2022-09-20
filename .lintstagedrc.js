f1b47011b9ab7fce6aa26486ec3cc011aef1afaa
Yuta Ohira <alesion90@gmail.com>
2022-09-20 09:37:55 +0900

    husky・lint-stagedを削除

--- .lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '**/*.{js,jsx,ts,tsx}': [
-    (filenames) => `next lint --fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
-    'prettier --write',
-  ],
-  '*.{html,json,css,scss}': ['yarn format'],
-};
