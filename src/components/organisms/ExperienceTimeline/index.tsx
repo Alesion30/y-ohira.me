7590dcbe8d3808bc585c12af3081f506f6e52fb3
Yuta Ohira <alesion90@gmail.com>
2022-09-24 22:07:58 +0900

    タイムラインに画像を表示

--- src/components/organisms/ExperienceTimeline/index.tsx
+++ src/components/organisms/ExperienceTimeline/index.tsx
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs'
+import NextImage, { StaticImageData } from 'next/image'
 import { FC } from 'react'
+
+import kyusyuUniversity from '/public/img/experience/kyushuUniversity.jpg'
 
 import styles from './style.module.scss'
 
@@ -7,6 +10,7 @@ type Experience = {
   title: string
   description: string
   date: Date
+  images: StaticImageData[]
 }
 
 const experiences: Experience[] = [
@@ -14,11 +18,13 @@ const experiences: Experience[] = [
     title: '九州大学大学システム情報科学府に入学',
     description: '九州大学大学システム情報科学府に入学しました',
     date: new Date(2022, 4, 1),
+    images: [kyusyuUniversity, kyusyuUniversity],
   },
   {
     title: '九州大学大学システム情報科学府に入学',
     description: '九州大学大学システム情報科学府に入学しました',
     date: new Date(2022, 4, 1),
+    images: [],
   },
 ]
 
@@ -27,12 +33,13 @@ export const ExperienceTimeline: FC = () => {
     <ul className={styles.timeline}>
       {experiences.map((experience, i) => {
         const id = `experience_${i}`
-        const { title, description, date } = experience
+        const { title, description, date, images } = experience
 
         return (
           <li key={id} className={styles.timelineItem}>
             <input
               className={styles.radio}
+              defaultChecked={i === 0}
               id={id}
               name='timeline'
               type='radio'
@@ -46,6 +53,18 @@ export const ExperienceTimeline: FC = () => {
               </span>
               <span className={styles.circle} />
               <div className={styles.moreContent}>
+                {images.length > 0 && (
+                  <div className={styles.images}>
+                    {images.map((image, j) => (
+                      <NextImage
+                        key={`${id}_image_${j}`}
+                        height={300}
+                        src={image}
+                        width={400}
+                      />
+                    ))}
+                  </div>
+                )}
                 <p>{description}</p>
               </div>
             </div>
--- src/components/organisms/ExperienceTimeline/style.module.scss.d.ts
+++ src/components/organisms/ExperienceTimeline/style.module.scss.d.ts
@@ -1,6 +1,7 @@
 export const circle: string
 export const content: string
 export const date: string
+export const images: string
 export const label: string
 export const moreContent: string
 export const radio: string
